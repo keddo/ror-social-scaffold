7eb9146757d9795714e783c1f274976fe0d68056
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-10-15 12:04:31 +0200

    Create comment_spec.rb

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'validate' do
+    it 'the presence of content' do
+        @user = User.create!(name: 'Test Name', email: 'test@email.com', gravatar_url: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y')
+
+        @post = Post.create!(user_id: @user.id, content: 'this is a post! hi!')
+
+      expect(comment.valid?).to be true
+    end
+
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:post_id) }
+    it { should validate_presence_of(:content) }
+    it { should validate_length_of(:content).is_at_most(200).with_long_message('200 characters in comment is the maximum allowed.') }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+end
