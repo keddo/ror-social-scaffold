c1e89721a12c9764bb6f4653bc8c99d24ced59cc
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-10-16 15:50:19 +0200

    Add accept and reject button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -9,7 +9,20 @@ class FriendshipsController < ApplicationController
       redirect_to friendships_path
     end
 
-    private 
+    def confirm
+      @user = User.find(params[:friendship_id])
+      current_user.confirm_friend(@user)
+      flash[:notice] = "Congrats! You added #{@user.name} to your friend list!"
+      redirect_to request.referrer
+    end
+
+    def reject
+      @friendship = Friendship.find_by(user_id: params[:friendship_id])
+      flash[:notice] = "You canceled #{@user.name} friend request!" if @friendship.destroy
+      redirect_to request.referrer
+    end
+
+    private
     def friend_params
       params.permit(:user_id, :friend_id)
     end
