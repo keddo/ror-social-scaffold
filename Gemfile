b34e5506ca9fa73d56a90d420cf87be203af2000
keddo <adu.kedir.abdurahman@gmail.com>
2020-10-17 09:32:19 +0300

    Fix linting errors

--- Gemfile
+++ Gemfile
@@ -65,8 +65,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'rubocop', '~>0.81.0'
-  gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'web-console', '>= 3.3.0'
 end
 
--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,9 +5,9 @@ class FriendshipsController < ApplicationController
 
   def create
     @friendship = Friendship.new(friend_params)
-    return if !@friendship.save
+    return unless @friendship.save
 
-    redirect_to friendships_path
+    redirect_to request.referrer, notice: "Request was successfully sent to #{@friendship.friend.name}"
   end
 
   def confirm
@@ -20,6 +20,15 @@ class FriendshipsController < ApplicationController
   def reject
     @friendship = Friendship.find_by(user_id: params[:friendship_id])
     flash[:notice] = "You canceled #{@user.name} friend request!" if @friendship.destroy
+    redirect_to request.referrer
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @friendship1 = Friendship.find_by(user_id: current_user.id, friend_id: params[:id])
+    @friendship2 = Friendship.find_by(user_id: params[:id], friend_id: current_user.id)
+
+    flash[:alert] = "You have unfriended #{@user.name}" if @friendship1.destroy && @friendship2.destroy
     redirect_to request.referrer
   end
 
